9b8f3223e919510c90c58ee18a9bf923f42aa09d
Dallin Gilbert <djgilb24@byui.edu>
2020-05-15 15:11:04 -0600

    Adding Afghanistan message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("GOOOOOOOOOOOOD MOOOOORNING VIETNAAAAAAM");
+	System.out.println("GOOOOOOOOOOOOD MOOOOORNING FROM AFGHANISTAAAAN");
     }
 }
