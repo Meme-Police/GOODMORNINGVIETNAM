fdc92ed9c80cb5651dec12db69c806405d1599b9
swish4 <san17001@byui.edu>
2020-05-16 21:23:51 -0600

    Savannah's edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("GOOOOOOOOOOOOD MOOOOORNING VIETNAAAAAAM");
 	System.out.println("GOOOOOOOOOOOOD MOOOOORNING FROM AFGHANISTAAAAN");
+	System.out.println("Blood alone moves the wheels of history!");
     }
 }
